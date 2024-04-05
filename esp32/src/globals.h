a3a1760b155e5c1f762346e1ad8ab68ae6acfd69
angelocordero <johnangelocordero.dev@gmail.com>
2024-04-06 02:59:05 +0800

    even more fixes(3)

--- esp32/src/globals.h
+++ esp32/src/globals.h
@@ -4,7 +4,7 @@
 #define GLOBALS_h
 
 Coordinates currentLoc = Coordinates(999, 999);
-Coordinates targetLoc = Coordinates(10.7561422, 122.5448899);
+Coordinates targetLoc = Coordinates(10.7296723, 122.5590030);
 
 float heading = 0;
 float bearingToTarget = 999;
